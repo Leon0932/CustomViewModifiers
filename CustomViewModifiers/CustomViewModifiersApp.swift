47c97672dfc05761bbb02c59bad16d11fa95beb6
leonchamoun <leon.chamoun@icloud.com>
2025-01-04 20:12:53 +0100

    Initial Commit

---
+++ CustomViewModifiers/CustomViewModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewModifiersApp.swift
+//  CustomViewModifiers
+//
+//  Created by Leon Chamoun on 04.01.25.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
