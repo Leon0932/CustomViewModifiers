14586c074bc599aa1e55c94347f98b44cbc4ff32
leonchamoun <leon.chamoun@icloud.com>
2025-01-04 20:46:29 +0100

    Added Colors and renamed file path

---
+++ CustomViewModifiersAndStyles/Extension/Color+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extension.swift
+//  CustomViewModifiersAndStyles
+//
+//  Created by Leon Chamoun on 04.01.25.
+//
+
+import SwiftUI
+
+extension Color {
+    // Gray colors
+    static let ultraLightGray = Color.gray.opacity(0.1)
+    static let lightGray = Color.gray.opacity(0.2)
+}
