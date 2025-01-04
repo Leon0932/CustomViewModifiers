c6b994e75549e9c799d2ed82b8703f4435e4afa5
leonchamoun <leon.chamoun@icloud.com>
2025-01-04 20:46:54 +0100

    Added Button and Styles to ContentView

--- CustomViewModifiersAndStyles/ContentView.swift
+++ CustomViewModifiersAndStyles/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 25) {
+                createButtonList(title: "Primary", style: .primary)
+                createButtonList(title: "Secondary", style: .secondary)
+                createButtonList(title: "Standard", style: .standard)
+            }
         }
-        .padding()
+    }
+    
+    func createButtonList<Style: ButtonStyle>(title: String, style: Style) -> some View {
+        VStack {
+            Text("\(title) Buttons")
+                .font(.title)
+            
+            CustomButtonView(title: "Hello World!",
+                             buttonStyle: style) {
+                print("Hello World!")
+            }
+            
+            CustomButtonView(title: "Encrypt",
+                             icon: "lock.fill",
+                             buttonStyle: style) {
+                print("Encrypt Text!")
+            }
+            
+            CustomNavigationButton(title: "Navigation Button",
+                                   buttonStyle: style) {
+                Text("New View")
+            }
+        }
     }
 }
 
